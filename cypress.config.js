834403f6c351607dcc7dfaf8ac7d0f26d4171e06
Rebeca Gois <rebecagoismachado@gmail.com>
2024-10-17 02:56:05 -0300

    Corrige detecção de arquivos de teste Cypress e ajustes de configuração

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,6 @@
-// cypress.config.ts
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
+export default {
   e2e: {
-    setupNodeEvents(on, config) {
-      // Adicione qualquer lógica customizada aqui
-    },
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Certifique-se de que ele aceite arquivos .ts
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    baseUrl: "http://localhost:3000", 
   },
-});
+};
